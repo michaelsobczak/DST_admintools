0d3478a03c2c0d3b0ffa205a867e462f6ea20c17
Michael Sobczak <mikesobczak.code@gmail.com>
2020-07-21 09:16:07 -0400

    add example env and runserver script

---
+++ scripts/env.sh
@@ -0,0 +1,5 @@
+export DST_SERVER_NAME="Test Dedicated Server"
+export DST_CLUSTER_TOKEN="your token here"
+export DST_CLUSTER_DESCRIPTION="Test description for dedicated server"
+export DST_CLUSTER_INTENTION="cooperative"
+export DST_CLUSTER_PASSWORD="bottleneck"
